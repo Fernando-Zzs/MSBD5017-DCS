651ac5c4f9342a511e880e02e1b6ac49f5e4b71c
Ddanyu <yudan202209@163.com>
2023-11-04 11:49:50 +0800

    ui add ranking contton

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
                 tab.
               </p>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            {/* <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <SparklesIcon className="h-8 w-8 fill-secondary" />
               <p>
                 Experiment with{" "}
@@ -52,7 +52,7 @@ const Home: NextPage = () => {
                 </Link>{" "}
                 to build your own UI.
               </p>
-            </div>
+            </div> */}
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
@@ -61,6 +61,16 @@ const Home: NextPage = () => {
                   Block Explorer
                 </Link>{" "}
                 tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Experiment with{" "}
+                <Link href="/recipients-ranking" passHref className="link">
+                  Recipients Ranking
+                </Link>{" "}
+                to see the rank of Recipients.
               </p>
             </div>
           </div>
