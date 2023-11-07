301c6b00a6908c0eb3c372b7e24ffd0ca49f53a8
Ddanyu <yudan202209@163.com>
2023-11-07 15:34:05 +0800

    token

---
+++ packages/hardhat/contracts/myToken.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MSBD5017Token is ERC20 {
+    constructor() ERC20("MSBD5017", "M57") {
+        _mint(msg.sender, 1000);
+    }
+}
